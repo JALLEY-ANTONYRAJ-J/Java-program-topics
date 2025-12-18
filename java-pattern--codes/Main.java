1edb644a49be123f317d3c3a9835078a2cb30c5d
JALLEY-ANTONYRAJ-J <jalleyjalley011@gmail.com>
2025-12-18 12:53:29 +0530

    Add files via upload

---
+++ java-pattern--codes/Main.java
@@ -0,0 +1,386 @@
+
+//outbox pattern 
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i==1 || j==1 || i==n || j==n)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//outer box with x cross pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i==1 || j==1 || i==n || j==n || i==j || i+j==n+1)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+
+//right arrow pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i==1 ||  j==n ||  i+j==n+1)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//left arrow pattern
+
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i==1 ||  j==1 ||  i==j)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//bottom left arrow pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(j==1 ||  i==n ||  i+j==n+1)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//bottom right arrow pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i==n ||  j==n ||  i==j)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//left side pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i>=j)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+//left side offside pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(j>=i)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//right side pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i+j>=n+1)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//right offside pattern
+
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i+j<=n+1)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//upper traingle pattern
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 	for(int i=1;i<=n;i++){
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i+j>=n+1)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	for(int j=1;j<=n;j++){
+// 	    	    if(i>j)
+// 	    	        System.out.print("* ");
+// 	    	    else
+// 	    	         System.out.print("  ");
+// 	}
+// 	 System.out.println();
+	
+// 	}
+// }
+// }
+
+
+//traingle pattern with  number 
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+	
+// 		for(int i=1;i<=n;i++){
+// 		        int k=0;
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i+j>=n+1)
+// 	    	        System.out.print(++k +" ");
+// 	    	else
+// 	    	    System.out.print("  ");
+	    	
+// 	    	}
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(i>j){
+// 	    	        System.out.print(--k +" ");
+// 	    	}else{
+// 	    	    System.out.print("  ");
+// 	    	}
+
+
+	
+// 	}	 System.out.println();
+// }
+// }
+// }
+
+
+//number increament pattern 
+
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 	Scanner s=new Scanner(System.in);
+// 	int n=s.nextInt();
+// 	int l=0;
+// 		for(int i=1;i<=n;i++){
+		        
+// 	    	for(int j=1;j<=n;j++){
+// 	    	    if(true )
+// 	    	        System.out.printf("%02d ",++l);
+
+//         	}	
+//                  System.out.println();
+
+// }
+// }
+// }
+
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();   // use n = 5
+
+        for (int i = 0; i < n; i++) {
+
+            // J
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 2 || (i == 4 && j < 3))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // A
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == 2 || j == 0 || j == 4)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // L
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || i == 4)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // L
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || i == 4)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // E
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == 2 || i == 4 || j == 0)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // Y
+            for (int j = 0; j < n; j++) {
+                if ((i < 2 && (j == i || j == 4 - i)) || (i >= 2 && j == 2))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
